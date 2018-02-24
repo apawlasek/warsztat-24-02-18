631af96cdbba9efaf19ca0e9a6cfce5aa7c93188
apawlasek <anna.pawlasek@gmail.com>
2018-02-24 12:28:50 +0100

    primitives

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log('buka!')
+
+// data types in js
+
+//js jest dynamicznie typowany (opp. twardotypowany)
+
+//primitives:
+
+// string
+var textVariable = "huba";
+
+// number
+
+var numVariable = 1.3;
+
+//boolean
+
+var boolVariable = true;
+
+//null
+var boolVariable = null; //jest zdef, ale jest brak
+
+//undefined
+
+var boolVariable = undifined; //niezdefiniowany, nigdy nie istniał
+
+//end of non-objects.
+
